59f693c65bcd378cf5dacf54ac61e61c29ed4c6b
pauloandredm <pauloadmendonca@gmail.com>
2021-07-24 02:42:09 -0300

    Json use attempt

--- server/chat/views.py
+++ server/chat/views.py
@@ -1,19 +1,27 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.http import HttpResponse
+from django.http import HttpResponse,JsonResponse
 from django.views.generic import (View,TemplateView,
                                 ListView,DetailView,
                                 CreateView,DeleteView,
                                 UpdateView)
+from .models import Room
 from . import models
 
 
 # Create your views here.
 
 
-class RoomListView(ListView):
-    context_object_name = 'rooms'
-    model = models.Room
+def room_list_view(request):
+    rooms = Room.objects.all().values('name')  # or simply .values() to get all fields
+    room_list = list(rooms)  # important: convert the QuerySet to a list object
+    return JsonResponse(room_list, safe=False)
+    # https://simpleisbetterthancomplex.com/tutorial/2016/07/27/how-to-return-json-encoded-response.html
+
+
+# class RoomListView(ListView):
+#     context_object_name = 'rooms'
+#     model = models.Room
     # template_name = 'chat/index.html'
     
 
