580bc0d1edb1a1ced84423f8c1d330851fe5beb9
Felipe Chung <fchung.dev@gmail.com>
2021-07-27 10:18:25 -0300

    remove conflict with previous commit

--- client/src/components/Room/Room.js
+++ client/src/components/Room/Room.js
@@ -1,4 +1,4 @@
-import { React, useEffect } from "react";
+import { React } from "react";
 import { withRouter } from "react-router-dom";
 // import { Link } from "react-router-dom";
 import * as FaIcons from "react-icons/fa";
