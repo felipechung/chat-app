0819d175f74fe402479ca9bb55dd584bc54a4650
Felipe Chung <fchung.dev@gmail.com>
2021-07-27 10:10:31 -0300

    Revert "create chatbox component"
    This reverts commit 5ff15475eec7468352ebe1f55f563bf0566ab30f.

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,24 @@
+<<<<<<< HEAD
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import ChatBox from "./components/ChatBox/ChatBox";
 
 import HomePage from "./pages/HomePage";
+=======
+>>>>>>> parent of 5ff1547... create chatbox component
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+<<<<<<< HEAD
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/:id" component={ChatBox} />
       </Switch>
+=======
+      <h1>test</h1>
+>>>>>>> parent of 5ff1547... create chatbox component
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -4,9 +4,13 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
+<<<<<<< HEAD
 ReactDOM.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
   document.getElementById("root")
 );
+=======
+ReactDOM.render(<App />, document.getElementById("root"));
+>>>>>>> parent of 5ff1547... create chatbox component
